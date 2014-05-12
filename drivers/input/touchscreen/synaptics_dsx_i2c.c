7ac20b68772bee671fc7a865946ff5561c9f4db3
Steve Kondik <shade@chemlab.org>
2014-05-11 21:02:03 -0700

    input: Report timestamps from Synaptics driver
    Change-Id: I5509bb68a93b600ff3bb17b1761251024a6c2456

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -3052,13 +3052,18 @@ static void synaptics_rmi4_f1a_report(struct synaptics_rmi4_data *rmi4_data,
  * fingers detected.
  */
 static void synaptics_rmi4_report_touch(struct synaptics_rmi4_data *rmi4_data,
-		struct synaptics_rmi4_fn *fhandler)
+		struct synaptics_rmi4_fn *fhandler, const ktime_t timestamp)
 {
 	unsigned char touch_count_2d;
 
 	dev_dbg(&rmi4_data->i2c_client->dev,
 			"%s: Function %02x reporting\n",
 			__func__, fhandler->fn_number);
+
+	input_event(rmi4_data->input_dev, EV_SYN, SYN_TIME_SEC,
+			ktime_to_timespec(timestamp).tv_sec);
+	input_event(rmi4_data->input_dev, EV_SYN, SYN_TIME_NSEC,
+			ktime_to_timespec(timestamp).tv_nsec);
 
 	switch (fhandler->fn_number) {
 		case SYNAPTICS_RMI4_F11:
@@ -3158,7 +3163,8 @@ static void synaptics_rmi4_sensor_report(struct synaptics_rmi4_data *rmi4_data, 
 			{
 				if (fhandler->intr_mask &  intr[fhandler->intr_reg_num]) 
 				{
-					synaptics_rmi4_report_touch(rmi4_data, fhandler);
+					synaptics_rmi4_report_touch(rmi4_data, fhandler,
+							timestamp);
 				}
 			}
 		}
@@ -3196,9 +3202,10 @@ static void synaptics_rmi4_sensor_report(struct synaptics_rmi4_data *rmi4_data, 
 static irqreturn_t synaptics_rmi4_irq(int irq, void *data)
 {
 	struct synaptics_rmi4_data *rmi4_data = data;
+	ktime_t timestamp = ktime_get();
 
 	if (!rmi4_data->touch_stopped)
-		synaptics_rmi4_sensor_report(rmi4_data, ktime_get());
+		synaptics_rmi4_sensor_report(rmi4_data, timestamp);
 
 	return IRQ_HANDLED;
 }
